1f2e1ec75f646f01d92b3acc656bcdea390ef923
Gregor Wolf <gregor.wolf@gmail.com>
2022-07-31 18:20:06 +0200

    feat: hide standard create button

--- app/functions/webapp/ext/CustomController.controller.js
+++ app/functions/webapp/ext/CustomController.controller.js
@@ -20,6 +20,15 @@ sap.ui.define(
     "use strict";
     return ControllerExtension.extend("functions.ext.CustomController", {
       override: {
+        onAfterRendering: function () {
+          //allocations::AllocationsObjectPage--fe::table::inputFields::LineItem::View::StandardAction::Create
+          const createBtn = this.base.byId(
+            this.getView().getId() +
+              "--fe::table::inputFields::LineItem::InputFields::StandardAction::Create"
+          );
+          createBtn.unbindProperty("visible");
+          createBtn.setVisible(false);
+        },
         // 	/**
         // 	 * Called when a controller is instantiated and its View controls (if available) are already created.
         // 	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
